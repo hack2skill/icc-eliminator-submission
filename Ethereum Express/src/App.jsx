1b12a1c47144bc547bce9d523472e43d4dc620ef
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-03-02 04:21:13 +0530

    Update

--- Ethereum Express/src/App.jsx
+++ Ethereum Express/src/App.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { isWallectConnected } from "./Blockchain.Service";
-import { ToastContainer } from "react-toastify";
-import { checkStorage } from "./Cart.Service";
 import Header from "./components/Header";
 import Home from "./views/Home";
 import ShoppingCart from "./views/ShoppingCart";
 import SingleProduct from "./components/SingleProduct";
 import { setGlobalState, truncate, useGlobalState } from "./store";
 import Error from "./components/Error";
+import Status from "./components/Status";
 const App = () => {
   const [connectedAccount] = useGlobalState("connectedAccount");
   return (
@@ -35,6 +32,16 @@ const App = () => {
               element={
                 connectedAccount ? (
                   <SingleProduct />
+                ) : (
+                  <Error message={"Please Connect Your Wallet !!!"} />
+                )
+              }
+            />
+            <Route
+              path="/shopping/:id/status"
+              element={
+                connectedAccount ? (
+                  <Status />
                 ) : (
                   <Error message={"Please Connect Your Wallet !!!"} />
                 )
--- Ethereum Express/src/components/Error.jsx
+++ Ethereum Express/src/components/Error.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Error = ({ message }) => {
   return (
     <>
+    
       <div className="p-10 flex justify-center">
         <div
           className="flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
